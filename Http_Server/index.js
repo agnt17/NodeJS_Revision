28706acba5e82173b82cb3737170073607ba2adf
agnt17 <adityagupta20042003@gmail.com>
2025-01-21 18:39:14 +0530

    Urls

--- Http_Server/index.js
+++ Http_Server/index.js
@@ -1,20 +1,43 @@
 const http = require('http');
 const fs = require('fs');
+const { URL } = require('url'); // Modern URL handling
 
 const server = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} \n`;
-    fs.appendFile('./log.txt', log, (err, data)=>{
-        switch(req.url){
-            case "/":
-                res.end("This is a homepage");
-            case "/about":
-                res.end("This is an about page");
-            
-                
-        }
-    })
-})
+    if (req.url === '/favicon.ico') return res.end();
 
-server.listen(8000, ()=>{
-    console.log("Server is running at port 8000")
-})
+    const urlParams = new URL(req.url, `http://${req.headers.host}`); // Create a URL object
+    const log = `${Date.now()}: ${req.url}\n`;
+
+    // Append log asynchronously, separate from response logic
+    fs.appendFile('./log.txt', log, (err) => {
+        if (err) console.error("Error writing to log file:", err);
+    });
+
+    // Handle routes
+    switch (urlParams.pathname) {
+        case "/":
+            res.end("This is a homepage");
+            break;
+
+        case "/about":
+            res.end("This is an about page");
+            break;
+
+        case "/search":
+            const search = urlParams.searchParams.get('search_query');
+            if (search) {
+                res.end(`These are the results of the search: ${search}`);
+            } else {
+                res.end("Search query parameter is missing");
+            }
+            break;
+
+        default:
+            res.statusCode = 404;
+            res.end("404: Page not found");
+    }
+});
+
+server.listen(8000, () => {
+    console.log("Server is running at port 8000");
+});
